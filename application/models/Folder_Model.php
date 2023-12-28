05fef4c7d5cef4dc67f8d3d7fe9c8cf5ee5a45d7
“jessiropa” <kristinjessi9700@gmail.com>
2023-12-29 00:34:54 +0700

    model untuk insert folder dan insert link referensi

---
+++ application/models/Folder_Model.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Folder_Model extends CI_Model {
+    function getAllFolderLink(){
+        return $this->db->get('folder_referensi')->result_array();
+    }
+
+
+    function insertDataFolderLink($data){
+        return $this->db->insert('folder_referensi', $data);
+    }
+
+    function insertDataLink($data){
+        return $this->db->insert('link_referensi', $data);
+    }
+}
