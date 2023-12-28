03d6b3ef0577c52ca641136fca3058a31c0f370f
“jessiropa” <kristinjessi9700@gmail.com>
2023-12-29 00:37:22 +0700

    menambahkan function untuk insert folder dan insert data link ke database

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -3,6 +3,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller
 {
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('Folder_Model');
+    }
+
     public function index()
     {
         $data['judul'] = "DASHBOARD";
@@ -34,11 +39,39 @@ class Dashboard extends CI_Controller
 
     public function referensi()
     {
+        $data['folder'] = $this->Folder_Model->getAllFolderLInk();
         $data['judul'] = "REFERENSI";
         $this->load->view('templates/user_header', $data);
         $this->load->view('templates/user_topnav');
         $this->load->view('templates/user_sidebar');
-        $this->load->view('user/referensi');
+        $this->load->view('user/referensi', $data);
         $this->load->view('templates/user_footer');
+    }
+
+    public function tambahFolder(){
+        $namaFolder = $this->input->post('nama_folderlink');
+        $insertFolder = array(
+            'nama_folder' => $namaFolder
+        );
+        $this->Folder_Model->insertDataFolderLink($insertFolder);
+        redirect(base_url('dashboard/referensi'));
+    }
+
+    public function tambahLink(){
+        date_default_timezone_set('Asia/Jakarta');
+        $folder_link = $this->input->post('folder_link');
+        $judul_artikel = $this->input->post('judul_artikel');
+        $link_artikel = $this->input->post('link_artikel');
+        // var_dump($folder_link);
+
+        $data = array(
+            'nama_artikel' => $judul_artikel,
+            'link_artikel' => $link_artikel,
+            'kode_folder' => $folder_link,
+            'tgl_create' => date('Y-m-d')
+        );
+
+        $this->Folder_Model->insertDataLink($data);
+        redirect(base_url('dashboard/referensi'));
     }
 }
