028acaea098b4480771bed09746825380c1a2979
“jessiropa” <kristinjessi9700@gmail.com>
2023-12-28 20:45:25 +0700

    folder untuk view login

---
+++ application/views/login/login.php
@@ -0,0 +1,30 @@
+<div class="row">
+    <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
+        <div class="login-panel panel panel-default">
+            <div class="panel-heading">
+                <center>LOGIN - NOTED SKRIPSI</center>
+            </div>
+            <div class="panel-body">
+                <form role="form">
+                    <fieldset>
+                        <div class="form-group">
+                            <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus="">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                        </div>
+                        <div class="text">
+                            <a href="<?= base_url('login/registrasi'); ?>" class="small">Belum punya akun ? Registrasi akun</a>
+                            <!-- <a href="" class="small">Lupa kata sandi ?</a> -->
+                        </div>
+                        <div class="text">
+                            <!-- <a href="" class="small">Belum punya akun ? Registrasi akun</a> -->
+                            <a href="" class="small">Lupa kata sandi ?</a>
+                        </div>
+                        <a href="index.html" class="btn btn-primary" style="float: right">Login</a>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    </div><!-- /.col-->
+</div><!-- /.row -->
