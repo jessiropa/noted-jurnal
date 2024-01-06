23d57de5ff002145ae25c40a9364d5be73f362c2
jessiropa <kristinjessi9700@gmail.com>
2024-01-06 11:16:30 +0700

    menambahkan query untuk menampilkan all data link referensi

--- application/models/Folder_Model.php
+++ application/models/Folder_Model.php
@@ -1,16 +1,24 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-class Folder_Model extends CI_Model {
-    function getAllFolderLink(){
+defined('BASEPATH') or exit('No direct script access allowed');
+class Folder_Model extends CI_Model
+{
+    function getAllFolderLink()
+    {
         return $this->db->get('folder_referensi')->result_array();
     }
 
-
-    function insertDataFolderLink($data){
+    function insertDataFolderLink($data)
+    {
         return $this->db->insert('folder_referensi', $data);
     }
 
-    function insertDataLink($data){
+    function insertDataLink($data)
+    {
         return $this->db->insert('link_referensi', $data);
+    }
+
+    function getAllDataLink()
+    {
+        return $this->db->get('link_referensi')->result_array();
     }
 }
