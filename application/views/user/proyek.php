baf7522ae705935af4bdad2011e281b228b133ad
“jessiropa” <kristinjessi9700@gmail.com>
2024-01-09 06:54:50 +0700

    membuat view proyek.php

---
+++ application/views/user/proyek.php
@@ -0,0 +1,53 @@
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+    <div class="row">
+        <ol class="breadcrumb">
+            <li><a href="#">
+                    <em class="fa fa-folder-open"></em>
+                </a></li>
+            <li class="active">PROYEK</li>
+        </ol>
+    </div><!--/.row-->
+
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">PROYEK</h1>
+        </div>
+    </div><!--/.row-->
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Proyek Saya
+                    <span class="pull-right clickable panel-toggle panel-button-tab-left"><em class="fa fa-toggle-up"></em></span>
+                    <span class="pull-right panel-toggle panel-button-tab-left" id="modalFolder" onclick="create_folder()" style="background-color: #1E90FF;"><em class="fa fa-plus" style="color:white"></em></span>
+                </div>
+                <div class="panel-body">
+                    <table class="table">
+                        <thead class="thead-light">
+                            <tr>
+                                <th scope="col">Nama Proyek</th>
+                                <th scope="col">Tanggal dibuat proyek</th>
+                                <th scope="col">Pemilik</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Judul Proyek</td>
+                                <td>08-01-2024</td>
+                                <td>username</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="panel-footer">
+                    <br>
+                    <br>
+                </div>
+            </div>
+        </div><!--/.col-->
+        <div class="col-sm-12">
+            <p class="back-link">Lumino Theme by <a href="https://www.medialoot.com">Medialoot</a></p>
+        </div>
+    </div><!--/.row-->
+</div> <!--/.main-->
