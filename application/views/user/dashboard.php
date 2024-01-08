a5ab223c5b6bfe39ac73ed30dba9f39294f98419
jessiropa <kristinjessi9700@gmail.com>
2024-01-08 15:06:19 +0700

    mengubah beberapa tampilan pada dashboard

--- application/views/user/dashboard.php
+++ application/views/user/dashboard.php
@@ -17,45 +17,91 @@
     <div class="panel panel-container">
         <div class="row">
             <div class="col-md-12">
+                <span>
+                    <center> Senin, 8 Januari 2024</center>
+                </span>
                 <h1>
-                    <center>SELAMAT DATANG USER</center> <br>
+                    <center>Halo, Username</center> <br>
                 </h1>
             </div>
         </div><!--/.row-->
     </div>
 
-    <div class="row">
-        <div class="col-xs-6 col-md-3">
-            <div class="panel panel-default">
-                <div class="panel-body easypiechart-panel">
-                    <h4>New Orders</h4>
-                    <div class="easypiechart" id="easypiechart-blue" data-percent="92"><span class="percent">92%</span></div>
+    <div class="panel panel-container">
+        <div class="row">
+            <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+                <div class="panel panel-teal panel-widget border-right">
+                    <div class="row no-padding"><em class="fa fa-xl fa-shopping-cart color-blue"></em>
+                        <div class="large">120</div>
+                        <div class="text-muted">Tugas Selesai</div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-xs-6 col-md-3">
-            <div class="panel panel-default">
-                <div class="panel-body easypiechart-panel">
-                    <h4>Comments</h4>
-                    <div class="easypiechart" id="easypiechart-orange" data-percent="65"><span class="percent">65%</span></div>
+            <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+                <div class="panel panel-blue panel-widget border-right">
+                    <div class="row no-padding"><em class="fa fa-xl fa-comments color-orange"></em>
+                        <div class="large">52</div>
+                        <div class="text-muted">Tugas Terlambat</div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-xs-6 col-md-3">
-            <div class="panel panel-default">
-                <div class="panel-body easypiechart-panel">
-                    <h4>New Users</h4>
-                    <div class="easypiechart" id="easypiechart-teal" data-percent="56"><span class="percent">56%</span></div>
+            <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+                <div class="panel panel-orange panel-widget border-right">
+                    <div class="row no-padding"><em class="fa fa-xl fa-users color-teal"></em>
+                        <div class="large">24</div>
+                        <div class="text-muted">Tugas Tertunda</div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-xs-6 col-md-3">
-            <div class="panel panel-default">
-                <div class="panel-body easypiechart-panel">
-                    <h4>Visitors</h4>
-                    <div class="easypiechart" id="easypiechart-red" data-percent="27"><span class="percent">27%</span></div>
+            <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+                <div class="panel panel-red panel-widget ">
+                    <div class="row no-padding"><em class="fa fa-xl fa-search color-red"></em>
+                        <div class="large">25.2k</div>
+                        <div class="text-muted">Tugas Baru</div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div><!--/.row-->
+        </div><!--/.row-->
+    </div>
 </div> <!--/.main-->
+
+
+<script type='text/javascript'>
+    window.setTimeout("tanggal()", 100);
+
+
+    // function jam_tanggal() {
+    //     var dt = new Date();
+    //     if (dt.getTimezoneOffset() == 0) {
+    //         (a = dt.getTime() + (7 * 60 * 60 * 100))
+    //     } else {
+    //         (a = dt.getTime());
+    //     }
+    //     dt.setTime(a);
+    //     var tahun = dt.getFullYear();
+    //     var hari = dt.getDay();
+    //     var bulan = dt.getMonth();
+    //     var tanggal = dt.getDate();
+    //     var array_hari = new Array("Minggu, ", "Senin, ", "Selasa, ", "Rabu, ", "Kamis, ", "Jumat, ", "Sabtu, ");
+    //     var array_bulan = new Array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
+
+    //     document.getElementById("datetime").innerHTML = array_hari[hari] + " " + tanggal + " " + array_bulan[bulan] + " " + tahun + " | " + ((dt.getHours() < 10) ? "0" : "") + dt.getHours() + " : " + ((dt.getMinutes() < 10) ? "0" : "") + dt.getMinutes() + " : " + ((dt.getSeconds() < 10) ? "0" : "") + dt.getSeconds() + (" WIB");
+    //     jam_refresh();
+    // }
+
+    function jam_refresh() {
+        var refresh = 1000; // Refresh rate in milli seconds
+        mytime = setTimeout('jam_tanggal()', refresh)
+    }
+
+    function tanggal() {
+        var dt = new Date();
+        var hari = dt.getDay();
+        var bulan = dt.getMonth();
+        var tanggal = dt.getDate();
+        var array_hari = new Array("Minggu, ", "Senin, ", "Selasa, ", "Rabu, ", "Kamis, ", "Jumat, ", "Sabtu, ");
+        var array_bulan = new Array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
+        document.getElementById("tanggal").innerHTML = array_hari[hari] + " " + tanggal + " " + array_bulan[bulan] + " " + tahun;
+        jam_refresh();
+    }
+</script>
