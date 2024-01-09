0dc523bcd4afc046fff7369be1e5c0fd60b4f951
jessiropa <kristinjessi9700@gmail.com>
2024-01-09 15:22:57 +0700

    menampilkan data target dan menampilkan modal insert target

--- application/views/user/target.php
+++ application/views/user/target.php
@@ -2,7 +2,7 @@
     <div class="row">
         <ol class="breadcrumb">
             <li><a href="#">
-                    <em class="fa fa-navicon"></em>
+                    <em class="fa fa-bar-chart"></em>
                 </a></li>
             <li class="active">Target</li>
         </ol>
@@ -20,7 +20,7 @@
                 <div class="panel-heading">
                     Target Saya
                     <span class="pull-right clickable panel-toggle panel-button-tab-left"><em class="fa fa-toggle-up"></em></span>
-                    <span class="pull-right panel-toggle panel-button-tab-left" id="modalFolder" onclick="create_folder()" style="background-color: #1E90FF;"><em class="fa fa-plus" style="color:white"></em></span>
+                    <span class="pull-right panel-toggle panel-button-tab-left" id="modalFolder" onclick="create_target()" style="background-color: #1E90FF;"><em class="fa fa-plus" style="color:white"></em></span>
                 </div>
                 <div class="panel-body">
                     <table class="table">
@@ -28,17 +28,23 @@
                             <tr>
                                 <th scope="col">Nama Target</th>
                                 <th scope="col">Periode Waktu</th>
-                                <th scope="col">Progres</th>
+                                <!-- <th scope="col">Progres</th> -->
                                 <th scope="col">Pemilik</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>Judul Target</td>
-                                <td>3 bulan</td>
-                                <td>0%</td>
-                                <td>username</td>
-                            </tr>
+                            <?php
+                            foreach ($all_target as $at) :
+                            ?>
+                                <tr>
+                                    <td><?= $at['nama_target'] ?></td>
+                                    <td><?= $at['deadline_target'] ?></td>
+                                    <!-- <td>0%</td> -->
+                                    <td><?= $at['pemilik'] ?></td>
+                                </tr>
+                            <?php
+                            endforeach;
+                            ?>
                         </tbody>
                     </table>
                 </div>
@@ -55,8 +61,8 @@
 </div> <!--/.main-->
 
 
-<!-- MODAL BUAT FOLDER -->
-<div class="modal fade modal-dialog-centered" id="modalCreateForlderLink" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<!-- MODAL BUAT TARGET -->
+<div class="modal fade modal-dialog-centered" id="modalCreateTarget" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -65,20 +71,20 @@
                     <span aria-hidden="true">&times;</span>
                 </button> -->
             </div>
-            <form method="post" action="<?= base_url('dashboard/tambahFolder') ?>">
+            <form method="post" action="<?= base_url('dashboard/tambahTarget') ?>">
                 <div class="modal-body">
                     <p style="color:black;">Target apa yang ingin Kamu capai ?</p>
                     <div class="form-group">
-                        <label for="nama_folderlink" class="col-form-label">Judul target</label>
-                        <input type="text" class="form-control" id="nama_folderlink" name="nama_folderlink">
+                        <label for="nama_target" class="col-form-label">Judul target</label>
+                        <input type="text" class="form-control" id="nama_target" name="nama_target">
                     </div>
                     <div class="form-group">
-                        <label for="nama_folderlink" class="col-form-label">Pemilik target</label>
-                        <input type="text" class="form-control" id="nama_folderlink" name="nama_folderlink">
+                        <label for="user_target" class="col-form-label">Pemilik target</label>
+                        <input type="text" class="form-control" id="user_target" name="user_target">
                     </div>
                     <div class="form-group">
-                        <label for="nama_folderlink" class="col-form-label">Periode Waktu</label>
-                        <input type="date" class="form-control" id="nama_folderlink" name="nama_folderlink">
+                        <label for="tenggat_waktu" class="col-form-label">Periode Waktu</label>
+                        <input type="date" class="form-control" id="tenggat_waktu" name="tenggat_waktu">
                     </div>
                 </div>
                 <div class="modal-footer">
@@ -92,8 +98,8 @@
 
 <!-- javascript -->
 <script type="text/javascript">
-    function create_folder() {
-        $("#modalCreateForlderLink").modal('show');
+    function create_target() {
+        $("#modalCreateTarget").modal('show');
     }
 
     function edit_folder() {
