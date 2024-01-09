9e53ee90fcd3f4e5a98a725d3dc9367990dc3864
jessiropa <kristinjessi9700@gmail.com>
2024-01-09 15:18:29 +0700

    menambahkan function target dan function insert target

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -7,6 +7,7 @@ class Dashboard extends CI_Controller
     {
         parent::__construct();
         $this->load->model('Folder_Model');
+        $this->load->model('Target_Model');
     }
 
     public function index()
@@ -21,6 +22,7 @@ class Dashboard extends CI_Controller
 
     public function target()
     {
+        $data['all_target'] = $this->Target_Model->getAllDataTarget();
         $data['judul'] = "TARGET";
         $this->load->view('templates/user_header', $data);
         $this->load->view('templates/user_topnav');
@@ -71,13 +73,13 @@ class Dashboard extends CI_Controller
         redirect(base_url('dashboard/referensi'));
     }
 
+    // tambah link 
     public function tambahLink()
     {
         date_default_timezone_set('Asia/Jakarta');
         $folder_link = $this->input->post('folder_link');
         $judul_artikel = $this->input->post('judul_artikel');
         $link_artikel = $this->input->post('link_artikel');
-        // var_dump($folder_link);
 
         $data = array(
             'nama_artikel' => $judul_artikel,
@@ -88,5 +90,24 @@ class Dashboard extends CI_Controller
 
         $this->Folder_Model->insertDataLink($data);
         redirect(base_url('dashboard/referensi'));
+    }
+
+    // tambah target
+    public function tambahTarget()
+    {
+        date_default_timezone_set('Asia/Jakarta');
+        $nama_target = $this->input->post('nama_target');
+        $user_target = $this->input->post('user_target');
+        $tenggat_waktu = $this->input->post('tenggat_waktu');
+
+        $data = array(
+            'nama_target' => $nama_target,
+            'pemilik' => $user_target,
+            'deadline_target' => $tenggat_waktu,
+            'create_target' => date('Y-m-d')
+        );
+
+        $this->Target_Model->insertDataTarget($data);
+        redirect(base_url('dashboard/target'));
     }
 }
