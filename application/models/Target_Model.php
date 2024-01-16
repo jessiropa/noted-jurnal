56ed02bb6c2a966034ac23313250b6899ecf1d66
jessiropa <kristinjessi9700@gmail.com>
2024-01-16 17:31:59 +0700

    membuat query untuk update data target

--- application/models/Target_Model.php
+++ application/models/Target_Model.php
@@ -14,8 +14,16 @@ class Target_Model extends CI_Model
     {
         return $this->db->get('target')->result_array();
     }
+
+    // insert data link
     function insertDataLink($data)
     {
         return $this->db->insert('link_referensi', $data);
+    }
+
+    // update data target
+    function updateDataTarget($data)
+    {
+        // return $this->db->update('')
     }
 }
